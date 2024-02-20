0bd62fa4d32d2d3e3d8d66a8088d0cfac7903fdc
Kamila Zdybal <kamila.zdybal@empa.ch>
2024-02-20 17:58:09 +0100

    Update

--- pykitPIV/image.py
+++ pykitPIV/image.py
@@ -243,26 +243,31 @@ class Image:
             particles_with_gaussian_light = np.zeros((self.__particles.size_with_buffer[0], self.__particles.size_with_buffer[1]))
 
             # Compute particle coordinates on the current image:
-            (particle_height_coordinate, particle_width_coordinate) = np.where(self.__particles.particle_positions[i]==1)
+            particle_height_coordinate = self.__particles.particle_coordinates[i][0]
+            particle_width_coordinate = self.__particles.particle_coordinates[i][1]
 
             # Establish the peak intensity for each particle depending on its position with respect to the laser beam plane:
             particle_positions_off_laser_plane = laser_beam_thickness * np.random.rand(self.__particles.n_of_particles[i]) - laser_beam_thickness/2
             particle_position_relative_to_laser_centerline = np.abs(particle_positions_off_laser_plane) / (laser_beam_thickness/2)
             particle_peak_intensities = self.exposures_per_image[i] * maximum_intensity * np.exp(-0.5 * (particle_position_relative_to_laser_centerline**2 / laser_beam_shape**2))
 
-            # Add Gaussian blur to each particle location that mimics the particle size:
+            # Add Gaussian blur to each particle location that mimics the light reflect from a particle of a given size:
             for p in range(0,self.__particles.n_of_particles[i]):
 
-                px_c_height = int(np.floor(particle_height_coordinate[p]))
-                px_c_width = int(np.floor(particle_width_coordinate[p]))
-                ceil_of_particle_radius = int(np.ceil(self.__particles.particle_diameters[i][p]/2))
+                px_c_height = np.floor(particle_height_coordinate[p]).astype(int)
+                px_c_width = np.floor(particle_width_coordinate[p]).astype(int)
+                ceil_of_particle_radius = np.ceil(self.__particles.particle_diameters[i][p]/2).astype(int)
 
-                for h in range(px_c_height-ceil_of_particle_radius, px_c_height+ceil_of_particle_radius):
-                    for w in range(px_c_width-ceil_of_particle_radius, px_c_width+ceil_of_particle_radius):
+                for h in range(px_c_height-ceil_of_particle_radius, px_c_height+ceil_of_particle_radius+1):
+                    for w in range(px_c_width-ceil_of_particle_radius, px_c_width+ceil_of_particle_radius+1):
+
+                        # Only change the value of pixels that are within the image area:
                         if (h >= 0 and h < self.__particles.size_with_buffer[0]) and (w >= 0 and w < self.__particles.size_with_buffer[1]):
 
-                            coordinate_height = h + 0.5 - particle_height_coordinate[p]
-                            coordinate_width = w + 0.5 - particle_width_coordinate[p]
+                            # 0.5 is added because we are computing the distance from particle center to the center of each considered pixel:
+                            coordinate_height = particle_height_coordinate[p] - (h + 0.5)
+                            coordinate_width = particle_width_coordinate[p] - (w + 0.5)
+
                             particles_with_gaussian_light[h,w] = particles_with_gaussian_light[h,w] + self.compute_light_intensity_at_pixel(particle_peak_intensities[p],
                                                                                                                                             self.__particles.particle_diameters[i][p],
                                                                                                                                             coordinate_height,
