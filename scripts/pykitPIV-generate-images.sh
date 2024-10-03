631f3db0a1cb14f41f37b789504ad56fc342ee3b
kamilazdybal <kamilazdybal@gmail.com>
2024-10-03 11:42:42 +0200

    Update

--- scripts/pykitPIV-generate-images.sh
+++ scripts/pykitPIV-generate-images.sh
@@ -12,7 +12,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --exclusive
-#SBATCH --constraint=cpu
+#SBATCH --constraint=gpu
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export CRAY_CUDA_MPS=1
@@ -22,4 +22,4 @@ conda activate pykitPIV
 export NCCL_DEBUG=INFO
 export HDF5_USE_FILE_LOCKING=FALSE
 
-srun python pykitPIV-generate-images.py --n_images 100 --size_buffer 10 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --n_images 5000 --size_buffer 10 --image_height 256 --image_width 256
