254eb6969276a45fabea414df794587be5fd96a3
kamilazdybal <kamilazdybal@gmail.com>
2024-10-08 15:58:01 +0200

    All at once

--- scripts/pykitPIV-generate-images.sh
+++ scripts/pykitPIV-generate-images.sh
@@ -5,8 +5,8 @@
 #SBATCH --time=00:30:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kamilazdybal@gmail.com
-#SBATCH --job-name=pykitPIV-generate-images-dt-1
-#SBATCH --output=pykitPIV-generate-images-dt-1.txt
+#SBATCH --job-name=pykitPIV-generate-images
+#SBATCH --output=pykitPIV-generate-images.txt
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks-per-node=1
@@ -22,4 +22,18 @@ conda activate pykitPIV
 export NCCL_DEBUG=INFO
 export HDF5_USE_FILE_LOCKING=FALSE
 
-srun python pykitPIV-generate-images.py --dt 1.0 --n_images 20 --size_buffer 10 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 1.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 2.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 3.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 4.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 5.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 6.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 7.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 8.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 9.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 10.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 11.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 12.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 13.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 14.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 15.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
