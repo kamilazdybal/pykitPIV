3d273bd3ecd850e922c9682ec4e9841ee6b04de0
kamilazdybal <kamilazdybal@gmail.com>
2024-10-10 22:50:50 +0200

    Updated scripts

--- scripts/pykitPIV-generate-images-with-various-dt.sh
+++ scripts/pykitPIV-generate-images-with-various-dt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 #
 #SBATCH --account=em13
-#SBATCH --partition=debug
-#SBATCH --time=00:30:00
+#SBATCH --partition=normal
+#SBATCH --time=4:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kamilazdybal@gmail.com
-#SBATCH --job-name=pykitPIV-generate-images
-#SBATCH --output=pykitPIV-generate-images.txt
+#SBATCH --job-name=pykitPIV-generate-images-various-dt
+#SBATCH --output=pykitPIV-generate-images-various-dt.txt
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks-per-node=1
@@ -22,4 +22,4 @@ conda activate pykitPIV
 export NCCL_DEBUG=INFO
 export HDF5_USE_FILE_LOCKING=FALSE
 
-srun python pykitPIV-generate-images.py --dt 1.0 --n_images 5000 --size_buffer 20 --image_height 256 --image_width 256
+srun pykitPIV-generate-images-with-various-dt.py --n_images 333 --size_buffer 80 --image_height 256 --image_width 256
