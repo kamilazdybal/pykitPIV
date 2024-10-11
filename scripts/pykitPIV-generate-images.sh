16f0351f400ec71fb450851a75566a4a6d1757f3
kamilazdybal <kamilazdybal@gmail.com>
2024-10-11 18:06:14 +0200

    New cases

--- scripts/pykitPIV-generate-images.sh
+++ scripts/pykitPIV-generate-images.sh
@@ -22,18 +22,20 @@ conda activate pykitPIV
 export NCCL_DEBUG=INFO
 export HDF5_USE_FILE_LOCKING=FALSE
 
-srun python pykitPIV-generate-images.py --dt 0.001 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 0.01 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 0.1 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 0.5 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 1.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 1.5 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 2.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 3.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 4.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 5.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 6.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 7.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 8.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 9.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
-srun python pykitPIV-generate-images.py --dt 10.0 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 0.2 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 0.3 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 0.4 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 0.6 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 0.7 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 0.8 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 0.9 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 1.1 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 1.2 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 1.3 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 1.4 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 1.6 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 1.7 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 1.8 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 1.9 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 2.5 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 3.5 --n_images 10 --size_buffer 60 --image_height 256 --image_width 256
