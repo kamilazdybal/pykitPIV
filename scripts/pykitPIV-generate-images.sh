627cc1b0df0a8c72f1da4d0127e7725adc85d635
kamilazdybal <kamilazdybal@gmail.com>
2024-10-15 13:18:18 +0200

    Script update

--- scripts/pykitPIV-generate-images.sh
+++ scripts/pykitPIV-generate-images.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --account=em13
 #SBATCH --partition=normal
-#SBATCH --time=6:00:00
+#SBATCH --time=10:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kamilazdybal@gmail.com
 #SBATCH --job-name=pykitPIV-generate-images-high-seeding-density
@@ -22,4 +22,4 @@ conda activate pykitPIV
 export NCCL_DEBUG=INFO
 export HDF5_USE_FILE_LOCKING=FALSE
 
-srun python pykitPIV-generate-images.py --dt 1 --n_images 2000 --size_buffer 10 --image_height 256 --image_width 256
+srun python pykitPIV-generate-images.py --dt 1 --n_images 1000 --size_buffer 10 --image_height 512 --image_width 512
