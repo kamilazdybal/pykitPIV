dfd36dbcff54c868656de3fe56f8d82e8922e007
kamilazdybal <kamilazdybal@gmail.com>
2024-10-22 12:02:37 +0200

    Script update

--- scripts/pykitPIV-generate-images-with-various-dt.py
+++ scripts/pykitPIV-generate-images-with-various-dt.py
@@ -3,16 +3,29 @@ import argparse
 import time
 from pykitPIV import Particle, FlowField, Motion, Image
 
-#################################################################################################################################
+########################################################################################################################
 ## Argument parser
-#################################################################################################################################
+########################################################################################################################
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--n_images',           type=int,  default=333, metavar='NIMAGES', help='Number of images')
-parser.add_argument('--size_buffer',        type=int,  default=80, metavar='SIZEBUFFER', help='Image buffer size')
-parser.add_argument('--image_height',       type=int,  default=100, metavar='H', help='Image height')
-parser.add_argument('--image_width',        type=int,  default=100, metavar='W', help='Image width')
+parser.add_argument('--n_images',               type=int,       default=10,                     metavar='N')
+parser.add_argument('--size_buffer',            type=int,       default=10,                     metavar='b')
+parser.add_argument('--image_height',           type=int,       default=100,                    metavar='H')
+parser.add_argument('--image_width',            type=int,       default=100,                    metavar='W')
+parser.add_argument('--dt',                     type=float,     default=[1, 2, 3], nargs="+",   metavar='dt')
+parser.add_argument('--diameters',              type=float,     default=[2, 3], nargs="+",      metavar='D')
+parser.add_argument('--densities',              type=float,     default=[0.3, 0.31], nargs="+", metavar='rho')
+parser.add_argument('--diameter_std',           type=float,     default=0.5,                    metavar='D_std')
+parser.add_argument('--gaussian_filters',       type=float,     default=[10, 10.1], nargs="+",  metavar='GF')
+parser.add_argument('--n_gaussian_filter_iter', type=int,       default=10,                     metavar='n_GF_iter')
+parser.add_argument('--displacement',           type=float,     default=[2.0, 3.0], nargs="+",  metavar='disp')
+parser.add_argument('--n_steps',                type=int,       default=10,                     metavar='N_STEPS')
+parser.add_argument('--exposures',              type=float,     default=[0.9, 0.95], nargs="+", metavar='EXP')
+parser.add_argument('--laser_beam_thickness',   type=float,     default=1,                      metavar='LB-t')
+parser.add_argument('--laser_beam_shape',       type=float,     default=0.95,                   metavar='LB-s')
+parser.add_argument('--alpha_denominator',      type=float,     default=10,                     metavar='ALPHA')
+parser.add_argument('--random_seed',            type=int,       default=100,                    metavar='RS')
 
 args = parser.parse_args()
 
@@ -23,8 +36,26 @@ n_images = vars(args).get('n_images')
 size_buffer = vars(args).get('size_buffer')
 image_height = vars(args).get('image_height')
 image_width = vars(args).get('image_width')
+time_separations = list(vars(args).get('dt'))
+diameters_min, diameters_max = tuple(vars(args).get('diameters'))
+densities_min, densities_max = tuple(vars(args).get('densities'))
+diameter_std = vars(args).get('diameter_std')
+gaussian_filters_min, gaussian_filters_max = tuple(vars(args).get('gaussian_filters'))
+n_gaussian_filter_iter = vars(args).get('n_gaussian_filter_iter')
+displacement_min, displacement_max = tuple(vars(args).get('displacement'))
+n_steps = vars(args).get('n_steps')
+exposures_min, exposures_max = tuple(vars(args).get('exposures'))
+laser_beam_thickness = vars(args).get('laser_beam_thickness')
+laser_beam_shape = vars(args).get('laser_beam_shape')
+alpha_denominator = vars(args).get('alpha_denominator')
+random_seed = vars(args).get('random_seed')
 
 image_size = (image_height, image_width)
+
+print('Running for the following time separations:')
+print(time_separations)
+
+dts_string = '-'.join([str(i) for i in time_separations])
 
 #################################################################################################################################
 ## Generate images
@@ -32,48 +63,43 @@ image_size = (image_height, image_width)
 
 tic = time.perf_counter()
 
-time_separations = [1,2]
-
-for i, dt in enumerate(time_separations):
+for i, time_separation in enumerate(time_separations):
 
     print('Generating images for time separation of ' + str(dt) + 's...')
 
     particles = Particle(n_images,
                          size=image_size,
                          size_buffer=size_buffer,
-                         diameters=(1, 5),
-                         distances=(1, 2),
-                         densities=(0.1, 0.3),
-                         diameter_std=0.5,
-                         seeding_mode='random',
-                         random_seed=100)
+                         diameters=(diameters_min, diameters_max),
+                         densities=(densities_min, densities_max),
+                         diameter_std=diameter_std,
+                         random_seed=random_seed)
 
     flowfield = FlowField(n_images,
                           size=image_size,
                           size_buffer=size_buffer,
-                          random_seed=100)
+                          random_seed=random_seed)
 
-    flowfield.generate_random_velocity_field(gaussian_filters=(2, 20),
-                                             n_gaussian_filter_iter=10,
-                                             displacement=(2, 10))
+    flowfield.generate_random_velocity_field(gaussian_filters=(gaussian_filters_min, gaussian_filters_max),
+                                             n_gaussian_filter_iter=n_gaussian_filter_iter,
+                                             displacement=(displacement_min, displacement_max))
 
     motion = Motion(particles,
                     flowfield,
-                    time_separation=dt)
+                    time_separation=time_separation)
 
-    motion.runge_kutta_4th(n_steps=10)
+    motion.runge_kutta_4th(n_steps=n_steps)
 
-    image = Image(random_seed=100)
+    image = Image(random_seed=random_seed)
 
     image.add_particles(particles)
     image.add_flowfield(flowfield)
     image.add_motion(motion)
-    image.add_reflected_light(exposures=(0.5, 0.95),
+    image.add_reflected_light(exposures=(exposures_min, exposures_max),
                               maximum_intensity=2 ** 16 - 1,
-                              laser_beam_thickness=1,
-                              laser_over_exposure=1,
-                              laser_beam_shape=0.95,
-                              alpha=1 / 10)
+                              laser_beam_thickness=laser_beam_thickness,
+                              laser_beam_shape=laser_beam_shape,
+                              alpha=1 / alpha_denominator)
 
     images_I1 = image.remove_buffers(image.images_I1)
     images_I2 = image.remove_buffers(image.images_I2)
@@ -91,11 +117,11 @@ for i, dt in enumerate(time_separations):
         images_tensor = np.concatenate((images_tensor, current_images_tensor), axis=0)
         targets_tensor = np.concatenate((targets_tensor, current_targets_tensor), axis=0)
 
-tensors_dictionary = {"I"      : images_tensor, 
+tensors_dictionary = {"I"      : images_tensor,
                       "target" : targets_tensor}
 
-image.save_to_h5(tensors_dictionary, 
-                 filename='pykitPIV-dataset-' + str(n_images*len(time_separations)) + '-PIV-pairs-' + str(image_height) + '-by-' + str(image_width) + '-dt-1p0-2p0.h5',
+image.save_to_h5(tensors_dictionary,
+                 filename='pykitPIV-dataset-' + str(n_images*len(time_separations)) + '-PIV-pairs-' + str(image_height) + '-by-' + str(image_width) + '-dt-' + dts_string + '-rs-' + str(random_seed) + '.h5',
                  verbose=True)
 
 toc = time.perf_counter()
--- scripts/pykitPIV-generate-images-with-various-dt.sh
+++ scripts/pykitPIV-generate-images-with-various-dt.sh
@@ -22,4 +22,4 @@ conda activate pykitPIV
 export NCCL_DEBUG=INFO
 export HDF5_USE_FILE_LOCKING=FALSE
 
-srun python pykitPIV-generate-images-with-various-dt.py --n_images 5000 --size_buffer 20 --image_height 512 --image_width 512
+srun python pykitPIV-generate-images-with-various-dt.py -- dt 0.5 1 1.5 2 2.5 3 3.5 4 5 6 7 8 9 10 --n_images 357 --size_buffer 80 --image_height 256 --image_width 256
