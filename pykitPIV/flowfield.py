fce98339285c43ac9b40ccafdaddce256aa51de9
kamilazdybal <kamilazdybal@gmail.com>
2025-01-22 17:06:06 +0100

    Initialized a function for the Langevin model

--- pykitPIV/flowfield.py
+++ pykitPIV/flowfield.py
@@ -722,6 +722,132 @@ class FlowField:
             self.__velocity_field[i, 0, :, :] = velocity_field_u
             self.__velocity_field[i, 1, :, :] = velocity_field_v
 
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    def generate_langevin_velocity_field(self,
+                                         mean_field,
+                                         integral_time_scale=1,
+                                         sigma=1,
+                                         n_iterations=100):
+        """
+        Generates a velocity field using the simplified Langevin model (SLM) by solving the following stochastic
+        partial differential equation:
+
+        .. math::
+
+            dU^*(t) = - \\frac{1}{T_L} U^*(t) dt + \\sqrt{\\frac{1}{T_L} 2 \\sigma^2 }  d W(t)
+
+        where :math:`U^*(t)` is one component of particle's velocity, :math:`T_L` is the Lagrangian integral time scale,
+        :math:`\sigma` is the turbulent kinetic energy, and :math:`W(t)` is the stochastic variable.
+
+        In essence, the SLM model simulates stationary isotropic turbulence in the Lagrangian sense, where stochastic
+        particles are allowed to drift and carry fluid velocity to a different location.
+
+        In practice, we solve the following finite-difference set of equations for both components of velocity:
+
+        .. math::
+
+            u^*(t + \\Delta t) = u^*(t) - (u^*(t) - u_m^*) \\frac{\\Delta t}{T_L} + \\sqrt{\\frac{1}{T_L} 2 \\sigma^2 \\Delta t}  \\xi(t)
+
+            x(t + \\Delta t) = x(t) + u^*(t) \\Delta t
+
+        .. math::
+
+            v^*(t + \\Delta t) = v^*(t) - (v^*(t) - v_m^*) \\frac{\\Delta t}{T_L} + \\sqrt{\\frac{1}{T_L} 2 \\sigma^2 \\Delta t}  \\xi(t)
+
+            y(t + \\Delta t) = y(t) + v^*(t) \\Delta t
+
+        where :math:`u_m^*` and :math:`v_m^*` are the components of the mean velocity,
+        :math:`\\xi(t)` is a random variable :math:`\\xi(t) \in \mathcal{N}(0,1)`,
+        and :math:`x(t)` and :math:`y(t)` are coordinates of the stochastic particles.
+
+        The above equations define the update to velocity components and particle positions over :math:`n` iterations
+        (set by the parameter ``n_iterations``). :math:`\\Delta t` is then computed as:
+
+        .. math::
+
+            \\Delta t = \\text{int} \\Big( \\frac{T_L}{n} \\Big) + 1
+
+        Next, a per-pixel velocity is found as an average over the ensemble of stochastic particles present within that pixel.
+        In the case where particle drift makes any pixel empty, velocity component is interpolated.
+
+        Note that the stochastic particles for solving the SLM are in practice generated using the ``Particle`` class
+        but they are independent of the seeded particles used for PIV.
+
+        For a thorough discussion of SLM see section 12.3 of
+        `S.B. Pope - Turbulent Flows (1995) <https://www.cambridge.org/highereducation/books/turbulent-flows/C58EFF59AF9B81AE6CFAC9ED16486B3A#contents>`_.
+
+        **Example:**
+
+        .. code:: python
+
+            from pykitPIV import FlowField
+
+            # We are going to generate 10 flow fields for 10 PIV image pairs:
+            n_images = 10
+
+            # Specify size in pixels for each image:
+            image_size = (128,512)
+
+            # Initialize a flow field object:
+            flowfield = FlowField(n_images=n_images,
+                                  size=image_size,
+                                  size_buffer=10,
+                                  random_seed=100)
+
+            # Generate sinusoidal velocity field that will serve as the mean velocity field:
+            flowfield.generate_sinusoidal_velocity_field(amplitudes=(2, 4),
+                                                         wavelengths=(20,40),
+                                                         components='both')
+
+
+
+
+
+
+
+
+
+
+        :param mean_field:
+            ``numpy.ndarray`` specifying the mean velocity field with components :math:`u_m^*` and :math:`v_m^*`, respectively.
+            It should be of size :math:`(N, 2, H+2 \cdot b, W+2 \cdot b)`.
+            The SLM model essentially provides a stochastic drift over that user-specified mean velocity, preserving
+            the Lagrangian integral time scale and the turbulent kinetic energy. The mean velocity can be synthetically
+            generated using one of the velocity field generators provided in this class.
+        :param integral_time_scale: (optional)
+            ``int`` or ``float`` specifying the Lagrangian integral time scale, :math:`T_L`.
+        :param sigma: (optional)
+            ``int`` or ``float`` specifying the turbulent kinetic energy.
+        :param n_iterations: (optional)
+            ``int`` specifying the number of iterations, :math:`n`, during which the finite-difference equations are updated.
+        """
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        # Input parameter check:
+
+        if (not isinstance(mean_field, numpy.ndarray)):
+            raise ValueError("Parameter `mean_field` has to be of type 'numpy.ndarray'.")
+
+        if (not isinstance(integral_time_scale, int)) and (not isinstance(integral_time_scale, float)):
+            raise ValueError("Parameter `integral_time_scale` has to be of type 'int' or 'float'.")
+
+        if (not isinstance(sigma, int)) and (not isinstance(sigma, float)):
+            raise ValueError("Parameter `sigma` has to be of type 'int' or 'float'.")
+
+        if (not isinstance(n_iterations, int)):
+            raise ValueError("Parameter `n_iterations` has to be of type 'int'.")
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+
+
+
+
+
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     def upload_velocity_field(self, velocity_field):
