5cc33d7d14808373fcd9dc4d19090536592c43b2
kamilazdybal <kamilazdybal@gmail.com>
2025-04-05 16:13:17 +0200

    Added two new generic checks

--- pykitPIV/checks.py
+++ pykitPIV/checks.py
@@ -1,6 +1,22 @@
 import numpy as np
 import warnings
 
+def check_positive_int_or_float(x, name):
+
+    if not isinstance(x, int) and not isinstance(x, float):
+        raise ValueError("Parameter `" + name + "` has to be of type 'int' or 'float'.")
+
+    if x <= 0:
+        raise ValueError("Parameter `" + name + "` has to be a non-zero, positive value.")
+
+def check_non_negative_int_or_float(x, name):
+
+    if not isinstance(x, int) and not isinstance(x, float):
+        raise ValueError("Parameter `" + name + "` has to be of type 'int' or 'float'.")
+
+    if x < 0:
+        raise ValueError("Parameter `" + name + "` has to be a non-negative value.")
+
 def check_two_element_tuple(x, name):
 
     if not isinstance(x, tuple):
